761982a75d09452eb3d68e4f95c253c3cece2f55
Jawad Akhtar <jawad.akhtar@invozone.com>
2023-02-15 19:53:18 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>WATI Integration</p>
-        <a
-          className="App-link"
-          href="https://www.clare.ai/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clare.AI
-        </a>
-      </header>
-      <body></body>
+      <body>
+      </body>
     </div>
   );
 }
